c40a5a77a0f24d28122df0dff2b596d223618f3e
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2023-04-13 13:07:02 -0400

    Tarea_2

---
+++ person.js
@@ -0,0 +1,11 @@
+let cmAltura = 179;
+let mAltura = 1.79;
+let pesoKg = 93.54;
+let alturaRedondeadaArriba = Math.round(mAltura);
+let alturaRedondeadaAbajo = Math.floor(mAltura);
+let maxValor = Number.MAX_VALUE + 1;
+let esMaxIgual = maxValor === Number.MAX_VALUE;
+console.log(alturaRedondeadaArriba);
+console.log(alturaRedondeadaAbajo);
+console.log(maxValor);
+console.log(esMaxIgual);
